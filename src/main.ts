2c29b5a727d205a13817ed2f21bf9927e0543138
quangth <tranhungquang23497@gmail.com>
2023-10-17 17:25:55 +0700

    ci: Update cors urls

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
 
   // Cors
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:4000'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:4000',
+      'https://chat-x-black.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   });
