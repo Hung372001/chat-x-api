f615436d6114921507e55c3840c0e09612adb5c0
quangth <workerbee.2379@gmail.com>
2023-11-21 09:42:05 +0700

    fix: Update connect db config

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,12 +2,29 @@ import path from 'path';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 const dataSource = new DataSource({
+  keepConnectionAlive: true,
   type: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  port: Number(process.env.DB_PORT),
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  autoLoadEntities: true,
+  replication: {
+    master: {
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/../**/*.entity.js'],
+      synchronize: true,
+    },
+    slaves: [
+      {
+        host: process.env.DB_REPL_HOST,
+        port: +process.env.DB_REPL_PORT,
+        username: process.env.DB_REPL_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      },
+    ],
+  },
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/database/migrations/**/*{.ts,.js}'],
   migrationsTableName: 'migrations',
@@ -18,6 +35,9 @@ const dataSource = new DataSource({
   cli: {
     migrationsDir: 'src/database/migrations',
   },
+  logging: true,
+  logger: 'file',
+  maxQueryExecutionTime: 1000,
 } as DataSourceOptions);
 
 export default dataSource;
