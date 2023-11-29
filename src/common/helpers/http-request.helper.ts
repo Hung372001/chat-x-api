b83c310b17420ce345d93c600facf16219b4b373
quangth <workerbee.2379@gmail.com>
2023-11-29 16:55:20 +0700

    fix: Fix bug axios

--- src/common/helpers/http-request.helper.ts
+++ src/common/helpers/http-request.helper.ts
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 export const callSocket = (path: string, body: any) => {
   try {
-    axios.post(`${process.env.SOCKET_BASE_URL}/socket-gateway/${path}`, body, {
-      headers: { 'x-api-key': process.env.X_API_KEY },
-    });
+    axios
+      .post(`${process.env.SOCKET_BASE_URL}/socket-gateway/${path}`, body, {
+        headers: { 'x-api-key': process.env.X_API_KEY },
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
   } catch (e) {
     console.log(e);
   }
